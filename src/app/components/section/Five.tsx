e55034b96db1083287ee913811ae906556f18bdc
Zheka Grem <odinked@gmail.com>
2024-08-13 00:21:06 +0300

    add link 2 footer

--- src/app/components/section/Five.tsx
+++ src/app/components/section/Five.tsx
@@ -28,9 +28,7 @@ const Five = () => {
             </p>
           </motion.div>
           <motion.div variants={slideUp} initial="hidden" animate="visible" className={styles.img_1}>
-            <a href="https://x.com/LunamemeTeam">
-              <Image src="/join_us.png" width={531} height={417} alt="join us" />
-            </a>
+            <Image src="/join_us.png" width={531} height={417} alt="join us" />
           </motion.div>
           <motion.div variants={emergence} initial="hidden" animate="visible" className={styles.img_2}>
             <a href="https://x.com/LunamemeTeam" target="_blank" aria-label="X" title="X">
@@ -41,11 +39,11 @@ const Five = () => {
             <a href="https://t.me/lunameme1" target="_blank" aria-label="telegram" title="telegram">
               <Image src="/svg/telegram.svg" width={136} height={108} alt="telegram" />
             </a>
-            {/* <div className={styles.img_3_1}>
-              <a href="https://medium.com/@lunameme" target="_blank" aria-label="medium" title="medium">
-                <Image src="/m.png" width={136} height={108} alt="medium" />
-              </a>
-            </div> */}
+          </motion.div>
+          <motion.div variants={emergence} initial="hidden" animate="visible" className={styles.img_3_1}>
+            <a href="https://medium.com/@lunameme" target="_blank" aria-label="medium" title="medium">
+              <Image src="/m.png" width={136} height={108} alt="medium" />
+            </a>
           </motion.div>
           <motion.div variants={slideUp} initial="hidden" animate="visible" className={styles.img_4}>
             <Image src="/svg/dog3.svg" width={201} height={341} alt="x" />
